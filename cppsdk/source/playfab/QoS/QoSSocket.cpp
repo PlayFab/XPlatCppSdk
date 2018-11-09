2f8d2e11238354877020b654a1ba65cbe7b62c19
sanchitgarg <sanchit.garg0526@gmail.com>
2018-11-09 11:38:17 -0800

    Remove non-blocking/async from socket call since we are on a background thread. This could potentially cause the code to get stuck.

--- cppsdk/source/playfab/QoS/QoSSocket.cpp
+++ cppsdk/source/playfab/QoS/QoSSocket.cpp
@@ -35,13 +35,6 @@ namespace PlayFab
                 errorCode = xPlatSocket->SetTimeout(timeoutMs);
             }
 
-            // Set the blocking mode
-            if (errorCode == 0)
-            {
-                u_long mode = BLOCKING_ASYNC_MODE;
-                errorCode = xPlatSocket->SetMode(mode);
-            }
-
             // If the errorCode is not 0, return the last socket error.
             return (errorCode == 0) ? 0 : xPlatSocket->GetLastErrorCode();
         }
@@ -72,27 +65,13 @@ namespace PlayFab
             }
 
             // Wait for the reply.
-            while (true)
+            
+            if (xPlatSocket->ReceiveReply(buf, BUFLEN) == SOCKET_ERROR)
             {
-                int errorCode = xPlatSocket->ReceiveReply(buf, BUFLEN);
-                if (errorCode == SOCKET_ERROR)
-                {
-                    if ((errorCode = xPlatSocket->GetLastErrorCode()) == WSAEWOULDBLOCK)
-                    {
-                        std::this_thread::sleep_for(threadWaitTimespan);
-                    }
-                    else
-                    {
-                        result.errorCode = errorCode;
-                        LOG_QOS("recvfrom() failed with error code : " << result.errorCode << endl);
+                result.errorCode = xPlatSocket->GetLastErrorCode();
+                LOG_QOS("recvfrom() failed with error code : " << result.errorCode << endl);
 
-                        return std::move(result);
-                    }
-                }
-                else
-                {
-                    break;
-                }
+                return std::move(result);
             }
 
             // Snap the end time
