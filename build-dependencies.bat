feb5ada7b253ade7961159fa173266b35342730b
Vicodex <victorku@microsoft.com>
2018-11-13 11:00:58 -0800

    added deps submodules and deps readme files

---
+++ build-dependencies.bat
@@ -0,0 +1,7 @@
+@echo off
+echo === Build Xplat C++ SDK dependencies (BEGIN) ===
+echo ATTENTION!
+echo This batch file must be run in "x64 Native Tools Command Prompt for VS 2017"
+
+
+echo === Build Xplat C++ SDK dependencies (END) ===
