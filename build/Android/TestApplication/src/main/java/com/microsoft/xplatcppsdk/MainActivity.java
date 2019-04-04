15c79df8ef6c0eb4df26b23b9b7bd4dcd0c5b53e
Alex Lopatin (Design Laboratory Inc) <v-allopa@microsoft.com>
2019-04-03 18:15:47 -0700

    Project refactor. Now it's test app project and XplatCpp project

---
+++ build/Android/TestApplication/src/main/java/com/microsoft/xplatcppsdk/MainActivity.java
@@ -0,0 +1,17 @@
+package com.microsoft.xplatcppsdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    static {
+        System.loadLibrary("XplatCppSdk");
+    }
+}
