fa4c8a6daef1052196cdf633cd267d4f6fa9e2d0
Alonso Mondal <alonsomondal@microsoft.com>
2024-07-18 17:05:16 -0600

    Adding windows specific compiler directive for strncasecmp=_strnicmp def

--- code/source/playfab/PlayFabCurlHttpPlugin.cpp
+++ code/source/playfab/PlayFabCurlHttpPlugin.cpp
@@ -12,7 +12,9 @@ char(*__countof_helper(_CountofType(&_Array)[_SizeOfArray]))[_SizeOfArray];
 #define _countof(_Array) (sizeof(*__countof_helper(_Array)) + 0)
 #endif
 
-#define strncasecmp(x,y,z) _strnicmp(x,y,z)
+#ifdef _WIN32
+    #define strncasecmp(x,y,z) _strnicmp(x,y,z)
+#endif
 
 namespace PlayFab
 {
